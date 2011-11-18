fd1062c310d99fc653222ab881d69b07bd9501ca
yurodivuie <yurodivuie@gmail.com>
2011-11-17 21:56:26 -0800

    * Added a way for test systems to specify the default targets, rather than relying on a hard coded string.  I'm not sure if this the best method for doing this, since we're gathering a gradually expanding list of system properties.

--- src/main/java/com/dynacrongroup/webtest/WebDriverFactory.java
+++ src/main/java/com/dynacrongroup/webtest/WebDriverFactory.java
@@ -5,61 +5,68 @@ import java.util.List;
 
 import com.dynacrongroup.webtest.util.ConfigurationValue;
 
-/** This class figures out which WebDriver(s) to set up. */
+/**
+ * This class figures out which WebDriver(s) to set up.
+ */
 public class WebDriverFactory {
 
     public final static String WEBDRIVER_DRIVER = "WEBDRIVER_DRIVER";
     public final static String SINGLE_SAUCE = "SINGLE_SAUCE";
+    public final static String DEFAULT_TARGETS = "DEFAULT_TARGETS";
 
     public List<String[]> pair(String key, String value) {
-	List<String[]> results = new ArrayList<String[]>();
-	results.add(new String[] { key, value });
-	return results;
+        List<String[]> results = new ArrayList<String[]>();
+        results.add(new String[]{key, value});
+        return results;
     }
 
     public List<String[]> getDriverTargets() {
 
-	String selected = ConfigurationValue.getConfigurationValue(
-		WEBDRIVER_DRIVER, null);
-	if (selected != null) {
-	    return pair("byclass", selected);
-	}
+        String selected = ConfigurationValue.getConfigurationValue(
+                WEBDRIVER_DRIVER, null);
+        if (selected != null) {
+            return pair("byclass", selected);
+        }
 
-	String single_sauce = ConfigurationValue.getConfigurationValue(
-		SINGLE_SAUCE, null);
-	if (single_sauce != null) {
-	    String[] items = splitTarget(single_sauce);
-	    return pair(items[0], items[1]);
-	}
+        String single_sauce = ConfigurationValue.getConfigurationValue(
+                SINGLE_SAUCE, null);
+        if (single_sauce != null) {
+            String[] items = splitTarget(single_sauce);
+            return pair(items[0], items[1]);
+        }
 
-	return standardSauceLabsTargets();
+        return standardSauceLabsTargets();
     }
 
     /**
-     * These are the standard SauceLabs targets for running a multi-browser test
+     * These are the standard SauceLabs targets for running a multi-browser test.
+     * They can be configured using the DEFAULT_TARGETS config value.
+     *
+     * @return A list of string arrays; each list element is a paired browser/version.
      */
     private List<String[]> standardSauceLabsTargets() {
-	List<String[]> result = new ArrayList<String[]>();
+        List<String[]> result = new ArrayList<String[]>();
 
-	String[] defaultSauceLabsTargets = { "firefox:5", "iexplore:7",
-		"iexplore:8", "iexplore:9", "chrome:*" };
+        String[] defaultSauceLabsTargets = ConfigurationValue.getConfigurationValue(DEFAULT_TARGETS,
+                "firefox:5,iexplore:7,iexplore:8,iexplore:9,chrome:*").split(",");
 
-	for (String windowsBrowser : defaultSauceLabsTargets) {
-	    result.add(splitTarget(windowsBrowser));
-	}
+        for (String windowsBrowser : defaultSauceLabsTargets) {
+            result.add(splitTarget(windowsBrowser));
+        }
 
-	return result;
+        return result;
     }
-    
+
     /**
      * Splits a target string into browser/version String pair, with version set
      * to null if version is "*".
+     *
      * @param target a browser:version string (iexplore:8, for example)
      * @return target split into separate strings for browser and version.
      */
-    private String[] splitTarget( String target) {
+    private String[] splitTarget(String target) {
         String[] items = target.split(":");
         items[1] = items[1].replaceAll("\\*", "");  //* used to refer to "any browser".  Now null is used.
-        return new String[] {items[0], items[1]};
+        return new String[]{items[0], items[1]};
     }
 }
