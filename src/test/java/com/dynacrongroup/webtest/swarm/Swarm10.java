b2a4e2c44b8f59a6870da3bb9011509b92173115
David Ross Drake <yurodivuie@gmail.com>
2011-12-11 17:03:20 -0800

    Temporary commit of tests and fix for issue 4

---
+++ src/test/java/com/dynacrongroup/webtest/swarm/Swarm10.java
@@ -0,0 +1,39 @@
+package com.dynacrongroup.webtest.swarm;
+
+import com.dynacrongroup.webtest.ParallelRunner;
+import com.dynacrongroup.webtest.WebDriverBase;
+import com.dynacrongroup.webtest.util.Path;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.openqa.selenium.By;
+
+/**
+ * Test used to verify that parallel browsers can be created without crashing.
+ */
+@RunWith(ParallelRunner.class)
+public class Swarm10 extends WebDriverBase {
+
+
+
+    Path p = new Path("www.dynacrongroup.com", 80);
+
+    public Swarm10(String browser, String browserVersion) {
+        super(browser, browserVersion);
+    }
+
+                   @Test
+                   public void doSomething() {
+                       driver.get( p._(""));
+                       assertThat(driver.findElement(By.tagName("body")).getText(), containsString("Dynacron"));
+
+                   }
+
+                   @Test
+    public void doAnything() {
+        driver.get(p._(""));
+        assertThat(driver.findElement(By.tagName("body")).getText(), containsString("Welcome"));
+    }
+
+}
---
+++ src/test/java/com/dynacrongroup/webtest/swarm/Swarm6.java
@@ -0,0 +1,39 @@
+package com.dynacrongroup.webtest.swarm;
+
+import com.dynacrongroup.webtest.ParallelRunner;
+import com.dynacrongroup.webtest.WebDriverBase;
+import com.dynacrongroup.webtest.util.Path;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.openqa.selenium.By;
+
+/**
+ * Test used to verify that parallel browsers can be created without crashing.
+ */
+@RunWith(ParallelRunner.class)
+public class Swarm6 extends WebDriverBase {
+
+
+
+    Path p = new Path("www.dynacrongroup.com", 80);
+
+    public Swarm6(String browser, String browserVersion) {
+        super(browser, browserVersion);
+    }
+
+                   @Test
+                   public void doSomething() {
+                       driver.get( p._(""));
+                       assertThat(driver.findElement(By.tagName("body")).getText(), containsString("Dynacron"));
+
+                   }
+
+                   @Test
+    public void doAnything() {
+        driver.get(p._(""));
+        assertThat(driver.findElement(By.tagName("body")).getText(), containsString("Welcome"));
+    }
+
+}
