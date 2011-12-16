5047651481dd6a24e5bfe1dd97a02f57e2f7b715
David Ross Drake <yurodivuie@gmail.com>
2011-12-15 22:01:56 -0800

    * Running into trouble with version control; committing changes again.

--- src/main/java/com/dynacrongroup/webtest/WebDriverLauncher.java
+++ src/main/java/com/dynacrongroup/webtest/WebDriverLauncher.java
@@ -17,107 +17,120 @@ import com.dynacrongroup.webtest.util.ConnectionValidator;
  * browser.
  */
 public class WebDriverLauncher {
-	public static int MAX_RETRIES = 3;
-	/**
-	 * Unique identifier for this job run. Global value for entire suite
-	 * execution (i.e. corresponds to a single complete mvn clean verify)
-	 */
-	protected static String uniqueId = String.valueOf(UUID.randomUUID());
 
-	private String jobUrl = "";
+    public static int MAX_RETRIES = 3;
+    /**
+     * Unique identifier for this job run. Global value for entire suite
+     * execution (i.e. corresponds to a single complete mvn clean verify)
+     */
+    protected static String uniqueId = String.valueOf(UUID.randomUUID());
 
-	public String getJobUrl() {
-		return jobUrl;
-	}
+    private String jobUrl = "";
 
-	/**
-	 * Returns the actual web drivers. Requires the logger and target web
-	 * browser to be specified.
-	 */
-	public WebDriver getNewWebDriverInstance(String jobName, Logger testLog,
-			TargetWebBrowser target) {
+    public String getJobUrl() {
+        return jobUrl;
+    }
 
-		if (testLog == null) {
-			throw new IllegalArgumentException(
-					"No logger specified for the WebDriverLauncher.");
-		}
+    /**
+     * Returns the actual web drivers. Requires the logger and target web
+     * browser to be specified.
+     */
+    public WebDriver getNewWebDriverInstance(String jobName, Logger testLog,
+                                             TargetWebBrowser target) {
 
-		if (target == null) {
-			throw new IllegalArgumentException("No target browser specified.");
-		}
+        if (testLog == null) {
+            throw new IllegalArgumentException(
+                    "No logger specified for the WebDriverLauncher.");
+        }
 
-		if (target.isHtmlUnit()) {
-			testLog.debug("Initializing HTMLUnit: " + jobName);
+        if (target == null) {
+            throw new IllegalArgumentException("No target browser specified.");
+        }
 
-			return new HtmlUnitDriver(true);
+        if (target.isHtmlUnit()) {
+            testLog.debug("Initializing HTMLUnit: " + jobName);
 
-		}
+            return new HtmlUnitDriver(true);
 
-		if (target.isClassLoaded()) {
-			testLog.debug("Initializing WebDriver by specified class: "
-					+ jobName);
-			try {
-				return (WebDriver) Class.forName(target.version).newInstance();
-			} catch (InstantiationException e) {
-				testLog.error("Unable to load target WebDriver class.", e);
-			} catch (IllegalAccessException e) {
-				testLog.error("Unable to load target WebDriver class.", e);
-			} catch (ClassNotFoundException e) {
-				testLog.error("Unable to load target WebDriver class.", e);
-			}
-		}
+        }
 
-		WebDriver driver = null;
+        WebDriver driver = null;
 
-		boolean validSauceWebDriver = false;
-		int attempt = 0;
+        boolean validWebDriver = false;
+        
+        for (int attempt = 0; attempt < MAX_RETRIES && !validWebDriver; attempt++) {
+            testLog.debug("WebDriver provisioning attempt [{}]" + attempt +1 );
 
-		if (attempt < MAX_RETRIES && !validSauceWebDriver) {
-			attempt++;
+            if (target.isClassLoaded()) {
+                testLog.debug("Initializing WebDriver by specified class: "
+                        + jobName);
+                try {
+                    driver = (WebDriver) Class.forName(target.version).newInstance();
+                } catch (WebDriverException e) {
+                    testLog.error("Unable to load target WebDriver class.", e);     //Sometimes caused by port locking in FF
+                    if (e.getMessage().contains("Unable to bind to locking port")) {
+                        testLog.error("Locking port error may be caused by ephemereal port exhaustion.  Try reducing the number of threads.");
+                    }
+                }
+                // If this is not a WebDriverException caused by ephemereal port locking, it's a programmatic error that shouldn't be retried.
+                catch (InstantiationException e) {
+                    testLog.error("Unable to load target WebDriver class.", e);
+                    break;
+                } catch (IllegalAccessException e) {
+                    testLog.error("Unable to load target WebDriver class.", e);
+                    break;
+                } catch (ClassNotFoundException e) {
+                    testLog.error("Unable to load target WebDriver class.", e);
+                    break;
+                }
 
-			try {
-				testLog.debug("Initializing ondemand.saucelabs.com:80 job: "
-						+ jobName + " [" + uniqueId + "]");
+                validWebDriver = driver != null;
+            } else {
 
-				ConnectionValidator
-						.verifyConnection("http://ondemand.saucelabs.com/");
+                try {
+                    testLog.debug("Initializing ondemand.saucelabs.com:80 job: "
+                            + jobName + " [" + uniqueId + "]");
 
-				Platform platform = Platform.WINDOWS;
+                    ConnectionValidator
+                            .verifyConnection("http://ondemand.saucelabs.com/");
 
-				/** TODO SauceLabs-specific. May need to update in future. */
-				if (target.isInternetExplorer() && target.version.contains("9")) {
-					platform = Platform.VISTA;
-				}
-				DesiredCapabilities capabillities = new DesiredCapabilities(
-						target.browser, target.version, platform);
-				capabillities.setCapability("name", jobName);
-				capabillities.setCapability("tags", SystemName.getSystemName());
-				capabillities.setCapability("build", uniqueId);
-				driver = new RemoteWebDriver(
-						SauceLabsCredentials.getConnectionString(),
-						capabillities);
+                    Platform platform = Platform.WINDOWS;
 
-				if (driver.getWindowHandle() != null) {
-					validSauceWebDriver = true;
-					testLog.debug("Successfully launched SauceLabs WebDriver connection.");
-					jobUrl = "https://saucelabs.com/jobs/"
-							+ ((RemoteWebDriver) driver).getSessionId()
-									.toString();
-					testLog.trace("Job url set to: " + jobUrl);
-				} else {
-					testLog.warn("Unable to establish communication with SauceLabs on attempt "
-							+ attempt + " of " + MAX_RETRIES);
-				}
-			} catch (Throwable e) {
-				testLog.error("Unable to connect with SauceLabs on attempt "
-						+ attempt, e);
-			}
-		}
+                    /** TODO SauceLabs-specific. May need to update in future. */
+                    if (target.isInternetExplorer() && target.version.contains("9")) {
+                        platform = Platform.VISTA;
+                    }
+                    DesiredCapabilities capabillities = new DesiredCapabilities(
+                            target.browser, target.version, platform);
+                    capabillities.setCapability("name", jobName);
+                    capabillities.setCapability("tags", SystemName.getSystemName());
+                    capabillities.setCapability("build", uniqueId);
+                    driver = new RemoteWebDriver(
+                            SauceLabsCredentials.getConnectionString(),
+                            capabillities);
 
-		if (driver == null) {
-			throw new IllegalAccessError(
-					"Unable to establish connection to SauceLabs.");
-		}
-		return driver;
-	}
+                    if (driver.getWindowHandle() != null) {
+                        validWebDriver = true;
+                        testLog.debug("Successfully launched SauceLabs WebDriver connection.");
+                        jobUrl = "https://saucelabs.com/jobs/"
+                                + ((RemoteWebDriver) driver).getSessionId()
+                                .toString();
+                        testLog.trace("Job url set to: " + jobUrl);
+                    } else {
+                        testLog.warn("Unable to establish communication with SauceLabs on attempt "
+                                + attempt + " of " + MAX_RETRIES);
+                    }
+                } catch (Throwable e) {
+                    testLog.error("Unable to connect with SauceLabs on attempt "
+                            + attempt, e);
+                }
+            }
+        }
+
+        if (driver == null) {
+            throw new IllegalAccessError(
+                    "Unable to initialize valid WebDriver.");
+        }
+        return driver;
+    }
 }
